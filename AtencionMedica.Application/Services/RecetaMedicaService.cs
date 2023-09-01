811311dc8c21ed696a1fe35d1992ad7a0f837ddc
ricardoth <ricardotilleriaochoa@gmail.com>
2023-09-01 13:59:39 -0400

    RecetaMedicaService

--- AtencionMedica.Application/Services/RecetaMedicaService.cs
+++ AtencionMedica.Application/Services/RecetaMedicaService.cs
@@ -1,30 +1,105 @@
-﻿namespace AtencionMedica.Application.Services
+﻿using AtencionMedica.Domain.Entities;
+using AtencionMedica.Domain.Interfaces;
+
+namespace AtencionMedica.Application.Services
 {
     public class RecetaMedicaService : IRecetaMedicaService
     {
-        public Task<bool> Actualizar(RecetaMedica recetaMedica)
+        private readonly IRecetaMedicaRepository _recetaMedicaRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<RecetaMedica> _validator;
+        public RecetaMedicaService(IRecetaMedicaRepository recetaMedicaRepository, IUnitOfWork unitOfWork, IValidator<RecetaMedica> validator)
         {
-            throw new NotImplementedException();
+            _recetaMedicaRepository = recetaMedicaRepository;
+            _unitOfWork = unitOfWork;
+            _validator = validator;
         }
 
-        public Task Agregar(RecetaMedica recetaMedica)
+        public async Task<ICollection<RecetaMedica>> GetRecetaMedicas()
         {
-            throw new NotImplementedException();
+            var result = await _recetaMedicaRepository.GetAll();
+            if (result is null)
+                throw new BadRequestException(ErrorMessageStatus.NoRecordsFound);
+            return result;
         }
 
-        public Task<bool> Eliminar(int id)
+        public async Task<RecetaMedica> GetRecetaMedica(int id)
         {
-            throw new NotImplementedException();
+            var result = await _recetaMedicaRepository.GetById(id);
+            if (result is null)
+                throw new NotFoundException(ErrorMessageStatus.NotFound);
+            return result; 
         }
 
-        public Task<RecetaMedica> GetRecetaMedica(int id)
+        public async Task Agregar(RecetaMedica recetaMedica)
         {
-            throw new NotImplementedException();
+            var validationResult = _validator.Validate(recetaMedica);
+            if (!validationResult.IsValid)
+            {
+                var errores = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+                throw new ValidationResultException(errores);
+            }
+
+            try
+            {
+                await _unitOfWork.RecetaMedicaRepository.Add(recetaMedica);
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new BadRequestException(ErrorMessageStatus.CreateFailed);
+            }
         }
 
-        public Task<ICollection<RecetaMedica>> GetRecetaMedicas()
+        public async Task<bool> Actualizar(RecetaMedica recetaMedica)
         {
-            throw new NotImplementedException();
+            var validationResult = _validator.Validate(recetaMedica);
+            if (!validationResult.IsValid)
+            {
+                var errores = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+                throw new ValidationResultException(errores);
+            }
+
+            if (recetaMedica.Id <= 0)
+                throw new NotFoundException(ErrorMessageStatus.NotValidId);
+
+            try
+            {
+                var recetaMedicaBd = await _recetaMedicaRepository.GetById(recetaMedica.Id);
+                recetaMedicaBd.IdMedicamento = recetaMedica.IdMedicamento;
+                recetaMedicaBd.IdHistorialClinico = recetaMedica.IdHistorialClinico;
+                recetaMedicaBd.Instrucciones = recetaMedica.Instrucciones;
+                recetaMedicaBd.Cantidad = recetaMedica.Cantidad;
+                recetaMedicaBd.Observacion = recetaMedica.Observacion;
+                recetaMedicaBd.FecInicio = recetaMedica.FecInicio;
+                recetaMedicaBd.FecFin = recetaMedica.FecFin;
+                recetaMedicaBd.EsActivo = recetaMedica.EsActivo;
+
+                _unitOfWork.RecetaMedicaRepository.Update(recetaMedicaBd);
+                await _unitOfWork.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new BadRequestException(ErrorMessageStatus.UpdateFailed);
+            }
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            if (id <= 0)
+                throw new NotFoundException(ErrorMessageStatus.NotValidId);
+
+            try
+            {
+                await _unitOfWork.RecetaMedicaRepository.SoftDelete(id);
+                await _unitOfWork.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new BadRequestException(ErrorMessageStatus.DeleteFailed);
+            }
         }
     }
 }
---
+++ AtencionMedica.Application/Validators/RecetaMedicaValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace AtencionMedica.Application.Validators
+{
+    public class RecetaMedicaValidator : AbstractValidator<RecetaMedica>
+    {
+        public RecetaMedicaValidator()
+        {
+            RuleFor(e => e.IdHistorialClinico).NotNull().WithMessage("El IdHistorialClinico no puede ser núlo");
+            RuleFor(e => e.IdHistorialClinico).NotEmpty().WithMessage("El IdHistorialClinico no puede estar vacío");
+            RuleFor(e => e.IdHistorialClinico).NotEqual(0).WithMessage("El IdHistorialClinico no puede ser 0");
+
+            RuleFor(e => e.IdMedicamento).NotNull().WithMessage("El IdMedicamento no puede ser núlo");
+            RuleFor(e => e.IdMedicamento).NotEmpty().WithMessage("El IdMedicamento no puede estar vacío");
+            RuleFor(e => e.IdMedicamento).NotEqual(0).WithMessage("El IdMedicamento no puede ser 0");
+
+            RuleFor(e => e.Instrucciones).NotNull().WithMessage("Las Instrucciones no puede ser núlo");
+            RuleFor(e => e.Instrucciones).NotEmpty().WithMessage("Las Instrucciones no puede estar vacío");
+            RuleFor(e => e.Instrucciones).Length(3, 500).WithMessage("Las Instrucciones debe tener un mínimo de 3 y máximo 500 caractéres");
+
+            RuleFor(e => e.Cantidad).NotNull().WithMessage("La Cantidad no puede ser núlo");
+            RuleFor(e => e.Cantidad).NotEmpty().WithMessage("La Cantidad no puede estar vacío");
+            RuleFor(e => e.Cantidad).NotEqual(0).WithMessage("La Cantidad no puede ser 0");
+
+            RuleFor(e => e.FecInicio).NotNull().WithMessage("La FecInicio no puede ser núlo");
+            RuleFor(e => e.FecInicio).NotEmpty().WithMessage("La FecInicio no puede estar vacío");
+
+            RuleFor(e => e.FecFin).NotNull().WithMessage("La FecFin no puede ser núlo");
+            RuleFor(e => e.FecFin).NotEmpty().WithMessage("La FecFin no puede estar vacío");
+        }
+    }
+}
