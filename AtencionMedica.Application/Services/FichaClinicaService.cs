6a25f97d6e4147cc9e129797e62f4ae25c34b21e
ricardoth <ricardotilleriaochoa@gmail.com>
2023-09-03 01:33:58 -0300

    FichaClinicaService y validator

--- AtencionMedica.Application/Services/FichaClinicaService.cs
+++ AtencionMedica.Application/Services/FichaClinicaService.cs
@@ -2,29 +2,106 @@
 {
     public class FichaClinicaService : IFichaClinicaService
     {
-        public Task<bool> Actualizar(FichaClinica fichaClinica)
+        private readonly IFichaClinicaRepository _fichaClinicaRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<FichaClinica> _validator;
+
+        public FichaClinicaService(IFichaClinicaRepository fichaClinicaRepository,
+            IUnitOfWork unitOfWork,
+            IValidator<FichaClinica> validator)
         {
-            throw new NotImplementedException();
+            _fichaClinicaRepository = fichaClinicaRepository;
+            _unitOfWork = unitOfWork;
+            _validator = validator; 
         }
 
-        public Task Agregar(FichaClinica fichaClinica)
+        public async Task<FichaClinica> GetFichaClinica(int id)
         {
-            throw new NotImplementedException();
+            var result = await _fichaClinicaRepository.GetById(id);
+            if (result is null)
+                throw new NotFoundException(ErrorMessageStatus.NotFound);
+            return result;
         }
 
-        public Task<bool> Eliminar(int id)
+        public async Task<ICollection<FichaClinica>> GetFichasClinicas()
         {
-            throw new NotImplementedException();
+            var result = await _fichaClinicaRepository.GetAll();
+            if (result is null)
+                throw new BadRequestException(ErrorMessageStatus.NoRecordsFound);
+            return result;
         }
 
-        public Task<FichaClinica> GetFichaClinica(int id)
+        public async Task Agregar(FichaClinica fichaClinica)
         {
-            throw new NotImplementedException();
+            var validationResult = _validator.Validate(fichaClinica);
+            if (!validationResult.IsValid)
+            {
+                var errores = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+                throw new ValidationResultException(errores);
+            }
+
+            try
+            {
+                await _unitOfWork.FichaClinicaRepository.Add(fichaClinica);
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new BadRequestException(ErrorMessageStatus.CreateFailed);
+            }
         }
 
-        public Task<ICollection<FichaClinica>> GetFichasClinicas()
+        public async Task<bool> Actualizar(FichaClinica fichaClinica)
         {
-            throw new NotImplementedException();
+            var validationResult = _validator.Validate(fichaClinica);
+            if (!validationResult.IsValid)
+            {
+                var errores = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+                throw new ValidationResultException(errores);
+            }
+
+            if (fichaClinica.Id <= 0)
+                throw new NotFoundException(ErrorMessageStatus.NotValidId);
+
+            try
+            {
+                var fichaClinicaBd = await _fichaClinicaRepository.GetById(fichaClinica.Id);
+                fichaClinicaBd.IdPaciente = fichaClinica.IdPaciente;
+                fichaClinicaBd.IdMedico = fichaClinica.IdMedico;
+                fichaClinicaBd.IdModulo = fichaClinica.IdModulo;
+                fichaClinicaBd.IdEstadoFichaClinica = fichaClinica.IdEstadoFichaClinica;
+                fichaClinicaBd.IdPersonal = fichaClinica.IdPersonal;
+                fichaClinicaBd.FechaAtencion = fichaClinica.FechaAtencion;
+                fichaClinicaBd.FechaAtencion = fichaClinica.FechaAtencion;
+                fichaClinicaBd.EsActivo = fichaClinica.EsActivo;
+
+                _unitOfWork.FichaClinicaRepository.Update(fichaClinicaBd);
+                await _unitOfWork.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new BadRequestException(ErrorMessageStatus.UpdateFailed);
+            }
         }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            if (id <= 0)
+                throw new NotFoundException(ErrorMessageStatus.NotValidId);
+
+            try
+            {
+                await _unitOfWork.FichaClinicaRepository.SoftDelete(id);
+                await _unitOfWork.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new BadRequestException(ErrorMessageStatus.DeleteFailed);
+            }
+        }
+
+        
     }
 }
---
+++ AtencionMedica.Application/Validators/FichaClinicaValidator.cs
@@ -0,0 +1,32 @@
+﻿namespace AtencionMedica.Application.Validators
+{
+    public class FichaClinicaValidator : AbstractValidator<FichaClinica>
+    {
+        public FichaClinicaValidator()
+        {
+            RuleFor(e => e.IdPaciente).NotNull().WithMessage("El IdPaciente no puede ser núlo");
+            RuleFor(e => e.IdPaciente).NotEmpty().WithMessage("El IdPaciente no puede estar vacío");
+            RuleFor(e => e.IdPaciente).NotEqual(0).WithMessage("El IdPaciente no puede ser 0");
+
+            RuleFor(e => e.IdMedico).NotNull().WithMessage("El IdMedico no puede ser núlo");
+            RuleFor(e => e.IdMedico).NotEmpty().WithMessage("El IdMedico no puede estar vacío");
+            RuleFor(e => e.IdMedico).NotEqual(0).WithMessage("El IdMedico no puede ser 0");
+
+            RuleFor(e => e.IdPersonal).NotNull().WithMessage("El IdPersonal no puede ser núlo");
+            RuleFor(e => e.IdPersonal).NotEmpty().WithMessage("El IdPersonal no puede estar vacío");
+            RuleFor(e => e.IdPersonal).NotEqual(0).WithMessage("El IdPersonal no puede ser 0");
+            
+            RuleFor(e => e.IdEstadoFichaClinica).NotNull().WithMessage("El IdEstadoFichaClinica no puede ser núlo");
+            RuleFor(e => e.IdEstadoFichaClinica).NotEmpty().WithMessage("El IdEstadoFichaClinica no puede estar vacío");
+            RuleFor(e => e.IdEstadoFichaClinica).NotEqual(0).WithMessage("El IdEstadoFichaClinica no puede ser 0");
+
+            RuleFor(e => e.IdModulo).NotNull().WithMessage("El IdModulo no puede ser núlo");
+            RuleFor(e => e.IdModulo).NotEmpty().WithMessage("El IdModulo no puede estar vacío");
+            RuleFor(e => e.IdModulo).NotEqual(0).WithMessage("El IdModulo no puede ser 0");
+
+            RuleFor(e => e.FechaAtencion).NotNull().WithMessage("El FechaAtencion no puede ser núlo");
+            RuleFor(e => e.FechaAtencion).NotEmpty().WithMessage("El FechaAtencion no puede estar vacío");
+
+        }
+    }
+}
